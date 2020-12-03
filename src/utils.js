a4090961526882ff9b00ee2ee0757c10c6a87c4a
Marty Zalega <marty@zalega.me>
2020-12-03 11:28:07 +1000

    Fix up locale lookup by user agent language

--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,13 @@ import * as locales from 'date-fns/locale'
 
 const NUMBER_RE = /^\d+(\.\d+)?$/
 
+for (let key in locales) {
+  const lowerKey = key.toLowerCase()
+  if (!locales[lowerKey]) {
+    locales[lowerKey] = locales[key]
+  }
+}
+
 function isTimestamp(value) {
   switch (typeof(value)) {
     case 'number':
