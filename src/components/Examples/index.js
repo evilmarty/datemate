b9d188d6d7cdec128dcbedc4807659e76f7c24c1
Marty Zalega <marty@zalega.me>
2020-12-03 11:28:38 +1000

    Provide randomised examples

--- src/components/Examples/index.js
+++ src/components/Examples/index.js
@@ -1,15 +1,52 @@
 import { h } from 'preact'
+import { useMemo } from 'preact/hooks'
+import { sub, formatRelative, formatDistance } from 'date-fns'
 import style from './style.css'
 
-export default function() {
+const daysIndex = [0, 1, 2, 3, 4, 5, 6]
+const monthsIndex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+const defaultExamples = [
+  'Today',
+  'Tomorrow',
+  'Yesterday',
+]
+
+const defaultSuffixes = [
+  'Week',
+  'Month',
+  'Year',
+]
+
+function sample(items) {
+  const i = Math.floor(Math.random() * items.length)
+  return items[i]
+}
+
+export default function({ locale }) {
+  const examples = useMemo(() => {
+    const suffixes = [
+      ...defaultSuffixes,
+      ...daysIndex.map(locale.localize.day),
+      ...monthsIndex.map(locale.localize.month),
+    ]
+    const now = Date.now()
+
+    return [
+      ...defaultExamples,
+      `Last ${sample(suffixes)}`,
+      `Next ${sample(suffixes)}`,
+      formatDistance(sub(now, { hours: 1000 * Math.random() }), now, { locale, addSuffix: true }),
+      formatDistance(sub(now, { hours: -1000 * Math.random() }), now, { locale, addSuffix: true }),
+      formatRelative(sub(now, { hours: 100 * Math.random() }), now, { locale, addSuffix: true }),
+    ]
+  }, [locale])
+
   return (
     <ul className={style.container}>
-      <li className={style.item}><a href="#today">Today</a></li>
-      <li className={style.item}><a href="#tomorrow">Tomorrow</a></li>
-      <li className={style.item}><a href="#last%20week">Last week</a></li>
-      <li className={style.item}><a href="#next%20april">Next April</a></li>
-      <li className={style.item}><a href="#5%20hours%20from%20now">5 hours from now</a></li>
-      <li className={style.item}><a href="#november%205%201955">November 5 1955</a></li>
+      {examples.map(example => (
+        <li className={style.item}><a href={`#${encodeURIComponent(example)}`}>{example}</a></li>
+      ))}
     </ul>
   )
 }
