1b964e6309f67890468d7c9eb4dba821deb7d9b7
Marty Zalega <marty@zalega.me>
2022-06-14 16:35:29 +1000

    Add info dialog

--- src/components.tsx
+++ src/components.tsx
@@ -118,41 +118,98 @@ export function HeroIcon(props: HeroIconProps): Component {
   switch (local.name) {
     case 'check':
       return (
-        <svg xmlns="http://www.w3.org/2000/svg" {...others} width={24} height={24} viewBox="0 0 20 20" fill="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" {...others} width={24} height={24}  viewBox="0 0 24 24" fill="currentColor">
           <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
         </svg>
       )
     case 'x':
       return (
-        <svg xmlns="http://www.w3.org/2000/svg" {...others} width={24} height={24} viewBox="0 0 20 20" fill="none" stroke="currentColor" strokeWidth={2}>
+        <svg xmlns="http://www.w3.org/2000/svg" {...others} width={24} height={24}  viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
         </svg>
       )
     case 'exclamation-circle':
       return (
-        <svg xmlns="http://www.w3.org/2000/svg" {...others} width={24} height={24} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <svg xmlns="http://www.w3.org/2000/svg" {...others} width={24} height={24}  fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
       )
     case 'duplicate':
       return (
-        <svg xmlns="http://www.w3.org/2000/svg" {...others} width={24} height={24} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <svg xmlns="http://www.w3.org/2000/svg" {...others} width={24} height={24}  fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
         </svg>
       )
     case 'chevron-left':
       return (
-        <svg xmlns="http://www.w3.org/2000/svg" {...others} width={24} height={24} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <svg xmlns="http://www.w3.org/2000/svg" {...others} width={24} height={24}  fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
         </svg>
       )
     case 'chevron-right':
       return (
-        <svg xmlns="http://www.w3.org/2000/svg" {...others} width={24} height={24} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <svg xmlns="http://www.w3.org/2000/svg" {...others} width={24} height={24}  fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+        </svg>
+      )
+    case 'information-circle':
+      return (
+        <svg xmlns="http://www.w3.org/2000/svg" {...others} width={24} height={24}  fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+      )
+    case 'emoji-happy':
+      return (
+        <svg xmlns="http://www.w3.org/2000/svg" {...others} width={24} height={24} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
       )
     default:
       return null
   }
+}
+
+interface DialogProps {
+  modal?: boolean
+  open?: boolean
+  title?: string
+  icon?: string
+  children: Component[]
+  footer?: Component[]
+  onClose?: Function
+}
+
+export function Dialog(props: DialogProps): Component {
+  let el
+  createEffect(() => {
+    if (props.modal) {
+      el.showModal()
+    }
+    else if (props.open) {
+      el.show()
+    }
+    else {
+      el.close()
+    }
+  })
+  return (
+    <dialog onClose={props.onClose} onCancel={props.onClose} ref={el} class="relative p-0 bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
+      <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <div class="sm:flex sm:items-start">
+          <Show when={props.icon}>
+            <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+              <HeroIcon name={props.icon} class="h-6 w-6 text-blue-600"/>
+            </div>
+          </Show>
+          <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+            <h3 class="text-lg leading-6 font-medium text-gray-900">{props.title}</h3>
+            <div class="mt-2">{props.children}</div>
+          </div>
+        </div>
+      </div>
+      <Show when={props.footer}>
+        <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">{props.footer}</div>
+      </Show>
+    </dialog>
+  )
 }
