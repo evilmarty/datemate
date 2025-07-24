9616ed0567f4644b86d6750cd2b807cbef12d8e9
Marty Zalega <marty@zalega.me>
2025-07-24 12:49:41 +1000

    Make copy button optional

--- src/Field.tsx
+++ src/Field.tsx
@@ -15,30 +15,33 @@ const Field: React.FC<FieldProps> = ({
   align = "right",
   fieldName = null,
   className = null,
+  copyButton = true,
   ...rest
 }) => (
-  <div className={`${className} flex flex-col`}>
-    <div className="flex items-center text-gray-900 dark:text-gray-400 bg-gray-50 dark:bg-gray-950 border border-gray-300 dark:border-gray-900 rounded-md focus-within:ring-2 focus-within:text-black dark:focus-within:text-gray-300 focus-within:ring-blue-500 dark:focus-within:ring-blue-700">
-      {label && (
-        <label
-          htmlFor={fieldName || label}
-          className="text-sm font-medium text-gray-400 dark:text-gray-500 m-1 px-1 py-2"
-        >
-          {label}
-        </label>
-      )}
-      <Input
-        id={fieldName || label}
-        value={value}
-        className={`${ALIGNMENTS[align]} w-full`}
-        {...rest}
-      />
+  <div
+    className={`${className} flex items-center text-gray-900 dark:text-gray-400 bg-gray-50 dark:bg-gray-950 border border-gray-300 dark:border-gray-900 rounded-md focus-within:ring-2 focus-within:text-black dark:focus-within:text-gray-300 focus-within:ring-blue-500 dark:focus-within:ring-blue-700`}
+  >
+    {label && (
+      <label
+        htmlFor={fieldName || label}
+        className="text-sm font-medium text-gray-400 dark:text-gray-500 m-1 px-1 py-2"
+      >
+        {label}
+      </label>
+    )}
+    <Input
+      id={fieldName || label}
+      value={value}
+      className={`${ALIGNMENTS[align]} w-full`}
+      {...rest}
+    />
+    {copyButton && (
       <CopyButton
         value={String(value)}
         fieldName={fieldName || label || ""}
         className="m-1 p-1"
       />
-    </div>
+    )}
   </div>
 );
 
